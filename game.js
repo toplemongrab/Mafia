98eb11f161cde12b19f60afb1eb728050f9c102a
Solomon Jackson <45051954+sugarfi@users.noreply.github.com>
2019-01-15 10:00:29 -0500

    Create game.js

---
+++ game.js
@@ -0,0 +1,24 @@
+var c = document.getElementById('c').getContext('2d');
+var width = 500;
+var height = 500;
+
+var x = 0;
+var y = 0;
+var hp = 100;
+var lives = [true, true. true];
+var inventory = [undefined, undefined, undefined];
+
+function draw() {
+  c.clearRect(0, 0, width, height);
+}
+
+function update() {
+}
+
+function main() {
+  update();
+  draw();
+  window.requestAnimationFrame(main);
+ }
+ 
+ main();
