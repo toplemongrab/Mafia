927baefe11f327d12f2e9ae84022f6910823b227
Solomon Jackson <45051954+sugarfi@users.noreply.github.com>
2019-01-16 15:18:26 -0500

    Update game.js

--- game.js
+++ game.js
@@ -4,13 +4,27 @@ var height = 300;
 
 var x = 0;
 var y = 0;
+var i;
+
 var hp = 100;
+var heart = new Image();
+heart.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png' //Use offical version when made
+var blackHeart = new Image();
+blackHeart.src = 'https://www.dictionary.com/e/wp-content/uploads/2018/09/black-heart.png' // Replace with offical to
 var lives = [true, true, true];
+
 var inventory = [undefined, undefined, undefined];
 
 function draw() {
   c.clearRect(0, 0, width, height);
-  for (var i = 0; i < inventory.length; i++) {
+  for (i = 0; i < lives.length; i++) {
+    if (lives[i]) {
+      c.drawImage(heart, i * 50, height - 55, 50, 50);
+    } else {
+      c.drawImage(blackHeart, i * 50, height - 55, 50, 50);
+    }
+  }
+  for (i = 0; i < inventory.length; i++) {
     c.strokeStyle = '#000000'
     c.strokeRect(i * 50 + 102, height - 55, 50, 50);
   }
