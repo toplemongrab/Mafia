df6be3fcd4523cfe56ec50f289bda866eedc8ac0
Solomon Jackson <45051954+sugarfi@users.noreply.github.com>
2019-01-16 19:11:27 -0500

    Update game.js

--- game.js
+++ game.js
@@ -18,7 +18,7 @@ var inventory = [undefined, undefined, undefined];
 var selectedItem = 0;
 
 players = []
-var socket = new io.Socket();
+var socket = new io.Socket(window.location.hostname);
 function recv(message) {
   console.log('Recieveing data...');
   console.log(message);
